a0d0c99fbc73ca77f2bd5057d3e839f4b870ea85
2021182046 <lope32@naver.com>
2023-09-21 12:20:25 +0900

    그리기 함수 추가 및 수정

--- character_grass.py
+++ character_grass.py
@@ -6,10 +6,11 @@ open_canvas()
 character = load_image('character.png')
 grass = load_image('grass.png')
 
-clear_canvas()
-grass.draw_now(400,30)
-character.draw_now(400,90)
-delay(1)
+def render_frame(x, y):
+    clear_canvas()
+    grass.draw_now(400,30)
+    character.draw_now(x,y)
+    delay(0.1)
 
 def run_circle():
     print('CIRCLE')
@@ -17,22 +18,16 @@ def run_circle():
     for deg in range(0, 360, 5):
         x = cx + r * math.cos(math.radians(deg))
         y = cy + r * math.sin(math.radians(deg))
-        clear_canvas()
-        grass.draw_now(400,30)
-        character.draw_now(x,y)
-        delay(0.1)
+        render_frame(x, y)
 
 def run_retangle():
     print('RETANGLE')
     #bottom line
     for x in range(50,750+1,5):
-        clear_canvas()
-        grass.draw_now(400,30)
-        character.draw_now(x,90)
-        delay(0.1)
+        render_frame(x,90) #xy 위치에 캐틱터 그리는 함수
 
 while True:
-    #run_circle()
+    run_circle()
     run_retangle()
     break
 
