c2cb15e85fd232efd4555c9c3a48bcfb6de424c9
2021182046 <lope32@naver.com>
2023-09-21 11:52:37 +0900

    원, 사각형 무한반복 프레임 형성

--- character_grass.py
+++ character_grass.py
@@ -8,11 +8,18 @@ grass = load_image('grass.png')
 clear_canvas()
 grass.draw_now(400,30)
 character.draw_now(400,90)
+delay(1)
 
+def run_circle():
+    print('CIRCLE')
+    pass
+def run_retangle():
+    print('RETANGLE')
+    pass
 
-
-
-
+while True:
+    run_circle()
+    run_retangle()
 
 
 
