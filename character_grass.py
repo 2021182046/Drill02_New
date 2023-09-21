8033125d10dd234fdba6c59282fade4c8f12be12
2021182046 <lope32@naver.com>
2023-09-21 11:42:51 +0900

    소년 출력완료

--- character_grass.py
+++ character_grass.py
@@ -2,6 +2,20 @@ from pico2d import *
 
 open_canvas()
 
-# fill here
+character = load_image('character.png')
+grass = load_image('grass.png')
+
+clear_canvas()
+grass.draw_now(400,30)
+character.draw_now(400,90)
+
+
+
+
+
+
+
+
+
 
 close_canvas()
