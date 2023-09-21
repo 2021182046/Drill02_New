81142d63fdea342021a8ee712fc5b005fde43b73
2021182046 <lope32@naver.com>
2023-09-21 12:35:05 +0900

    사각 운동 오른쪽줄 완료

--- character_grass.py
+++ character_grass.py
@@ -30,6 +30,10 @@ def run_retangle():
     for x in range(750, 50-1, -5):
         render_frame(x, 550)
 
+    #right line
+    for y in range(90, 550+1, 5):
+        render_frame(750, y)
+
 while True:
     run_circle()
     run_retangle()
